7185fe0cf1a394e4df4ec214ec4c4fe79b6ed7b8
Devang Ghorpade <dg310@enovate-it.com>
2025-08-05 12:29:28 +0530

    [Socket IO API]

--- server.js
+++ server.js
@@ -7,13 +7,11 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 const PORT = process.env.PORT || 8000;
-// Serve static files from 'public' folder
 app.use(express.static('public'));
 
 io.on('connection', (socket) => {
   console.log('New client connected');
 
-  // Send dummy data every 3 seconds
   const intervalId = setInterval(() => {
     const dummyData = {
       timestamp: new Date(),
@@ -30,4 +28,8 @@ io.on('connection', (socket) => {
 
 server.listen(PORT, () => {
   console.log('Server running on http://localhost:8000');
+});
+
+app.get('/', (req, res) => {
+  res.send("Hello World, Welome to Socket.io");
 });
